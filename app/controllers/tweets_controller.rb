69a8575cc49963d14a8a42b9294672ba68ed1eda
n0dam1 <minoru.noda.0527@gmail.com>
2018-06-18 20:28:08 +0900

    Tweetコントローラーの仮実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,12 @@
 class TweetsController < ApplicationController
   def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.save
+    redirect_to root_path
+  end
+
+  private
+
+  def tweet_params
   end
 end
