db801d4520ff37a48b38240eb3b5ea9333520b86
n0dam1 <minoru.noda.0527@gmail.com>
2018-06-26 17:42:58 +0900

    不要コントローラーを削除

--- app/controllers/tweets_controller.rb
+++
@@ -1,7 +0,0 @@
-class TweetsController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root to: 'images#new'
   resources :images, only: [:create]
-  resources :tweets, only: [:create]
 end
